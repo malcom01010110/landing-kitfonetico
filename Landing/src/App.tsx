203922cddc584e39cfe170377b6c5d77fd1032b8
malcom01010110 <christianmanrique1988@gmail.com>
2026-05-05 19:30:04 -0300

    Update App.tsx

--- Landing/src/App.tsx
+++ Landing/src/App.tsx
@@ -13,7 +13,7 @@ import { GuaranteeSection } from './components/GuaranteeSection';
 import { Testimonials } from './components/Testimonials';
 import { FAQSection } from './components/FAQSection';
 import { Footer } from './components/Footer';
-import Gracias from "./pages/gracias";
+import Gracias from "./pages/Gracias";
 export function App() {
   return (
     <div className="min-h-screen font-body text-body bg-cream selection:bg-accent/30">
