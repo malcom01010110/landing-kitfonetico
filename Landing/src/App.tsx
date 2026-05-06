3b447e7966744d92986f294864527b24d983cbd2
malcom01010110 <christianmanrique1988@gmail.com>
2026-05-05 21:52:57 -0300

    fix import Gracias uppercase

--- Landing/src/App.tsx
+++ Landing/src/App.tsx
@@ -16,7 +16,7 @@ import { Testimonials } from './components/Testimonials';
 import { FAQSection } from './components/FAQSection';
 import { Footer } from './components/Footer';
 
-import Gracias from './pages/gracias';
+import gracias from './pages/gracias';
 
 function Home() {
   return (
